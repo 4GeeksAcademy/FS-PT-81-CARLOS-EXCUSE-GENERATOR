4891f567018b35fbffab630e37c148af9418dcea
Carlos03tb <carlostb0318@gmail.com>
2024-10-11 09:14:53 +0000

    hecho

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +7,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomIndex = Math.floor(Math.random() * who.length);
+
+  console.log(
+    `${who[getRandomIndex]} ${action[getRandomIndex]} ${what[getRandomIndex]} ${when[getRandomIndex]}`
+  );
 };
